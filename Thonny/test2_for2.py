677d9fddd37fe65461611974c2cd76f2c03b3fb2
tglod <48178371+tglod@users.noreply.github.com>
2020-11-25 19:24:18 +0100

    Update test2_for2.py

--- Thonny/test2_for2.py
+++ Thonny/test2_for2.py
@@ -1,9 +1,10 @@
-seq = ("AGCTTTCGCCTCAAAGA");
+seq = ("AGCTTTCGCCTCAAAGA")
+
 #konwertowanie sekwencji na listę, w której każda litera ma swój indeks od 0 do n-1
 lst=list(seq)
-t1=seq[0];
-t2=seq[1];
-nxt=seq[2];
+t1=lst[0]
+t2=lst[1]
+nxt=lst[2]
 print(lst)
 for x in  range( 0,len(lst)):
     
@@ -11,10 +12,10 @@ for x in  range( 0,len(lst)):
         
     if(lst[x]=="G"):
         if(x==0):
-             lst[x+1]='C';
-        elif(x==len(lst)-1):
-             lst[x-1]='C';
+            lst[x+1]='C'
+        elif(x==len(lst)-1)
+            lst[x-1]='C'
         else:
-             lst[x-1]='C';
-             lst[x+1]='C';
+            lst[x-1]='C'
+            lst[x+1]='C'
 print(lst)
